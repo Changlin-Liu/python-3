7e64586a229f097f000790bad256fa636b8faad0
simple0426 <perfect_0426@qq.com>
2015-11-02 20:01:35 +0800

    a small scipts

---
+++ fib.py
@@ -0,0 +1,13 @@
+# def fib2(n):
+#     result = []
+#     a, b = 0, 1
+#     while a < n:
+#         result.append(a)
+#         a, b = b, a + b
+#     return result
+# print(fib2(100))
+def addition(a, b=1, c=3):
+    add = a + b + c
+    return add
+sum = addition(2)
+print(sum)
