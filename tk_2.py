db1690d68bc3fae70a19dd19ce9315f53af8c8b8
simple0426 <perfect_0426@qq.com>
2015-11-12 14:45:29 +0800

    learn more about tkinter module

---
+++ tk_2.py
@@ -0,0 +1,9 @@
+from tkinter import *
+# 初始化Tk
+root = Tk()
+# 创建一个lable标签
+label = Label(root, text='Hello Tkinter')
+# 显示label标签
+label.pack()
+# 进入消息循环
+root.mainloop()
---
+++ tk_label.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+#! -*- coding:utf-8 -*-
+from tkinter import *
+root = Tk()
+root.title('Lable test')
+# 默认情况下文本居中显示，通过justify设置文字左对齐
+# bg北京色，fg前景色，width、height背景宽高
+Label(root, text='www.quxiu8.com',
+    bg='yellow', width=30, height=4,
+    wraplength=100, justify="left").pack()
+# anchor设置文本左边显示，通过justify设置文字右对齐
+Label(root, text='m.quxiu8.com',
+    bg='red', width=35, height=4,
+    wraplength=100, anchor='w', justify='right').pack()
+# relief设置label外观，wraplength设置一行显示的字符数
+Label(root, text='mapp.quxiu8.com',
+    bg='blue', width=40, height=4,
+    wraplength=100, anchor='e', relief='ridge').pack()
+root.mainloop()
