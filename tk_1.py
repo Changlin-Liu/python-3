de4de6b5e11b8db4a918a5b806108fe8f0413dac
simple0426 <perfect_0426@qq.com>
2015-11-12 14:43:25 +0800

    add some comment

--- tk_1.py
+++ tk_1.py
@@ -1,31 +1,28 @@
 from tkinter import *
 import tkinter.messagebox as messagebox
 
-
 class Appliaction(Frame):
 
-
-    def __init__(self, master=None):
+    def __init__(self, master = None):
         Frame.__init__(self, master)
+        # 以类方法的形式，将子widget[application]加入到父容器Frame并显示
         self.pack()
+        # 以类方法的形式，将子widget[label/button]加入父容器并显示
         self.createWidgets()
 
-
     def createWidgets(self):
-        self.helloLabel = Label(self, text='Hello, world!')
-        self.helloLabel.pack()
-        self.quitButton = Button(self, text='Quit', command=self.quit)
-        self.quitButton.pack()
-    # def createWidgets(self):
-        # self.nameInput = Entry(self)
-        # self.nameInput.pack()
-        # self.altertButton = Button(self, text='Hello', command=self.hello)
-        # self.altertButton.pack()
+        self.nameInput = Entry(self)
+        # pack方法吧widget加入到父容器
+        self.nameInput.pack()
+        self.altertButton = Button(self, text='Hello', command=self.hello)
+        self.altertButton.pack()
 
-    # def hello(self):
-        # name = self.nameInput.get() or 'world'
-        # messagebox.showinfo('Message', 'Hello, %s' % name)
-
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        messagebox.showinfo('Message', 'Hello, %s' % name)
+# 实例化application
 app = Appliaction()
+# 设置窗口名称
 app.master.title('Hello World')
+# 设置消息主循环
 app.mainloop()
