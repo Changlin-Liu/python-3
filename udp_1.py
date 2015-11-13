415136ffc19453dff7d5b27cc6133b9656fac1f5
simple0426 <perfect_0426@qq.com>
2015-11-13 19:41:44 +0800

    add some commnet

--- udp_1.py
+++ udp_1.py
@@ -1,4 +1,8 @@
 import socket
+# udp服务端处理流程：
+# 绑定端口--》接受客户端数据【含地址】--》返回客户端数据
+# udp客户端处理流程：
+# 向指定服务端发送数据【含地址】--》接受数据
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('127.0.0.1', 9999))
 print('Bind UDP on 9999')
