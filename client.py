b3c2cbd8b1cc0d47b0b3b1b97369f4499512ac18
simple0426 <perfect_0426@qq.com>
2015-11-13 16:34:54 +0800

    add some tcp client comment

--- client.py
+++ client.py
@@ -1,4 +1,8 @@
 import socket, time
+"""
+客户端处理流程：
+建立连接--》发送数据--》接受数据--》关闭连接
+"""
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 9999))
 print(s.recv(1024).decode('utf-8'))
