099a507f6e09e1260648f9dedce1647bec5b1cc9
simple0426 <perfect_0426@qq.com>
2015-11-18 20:05:43 +0800

    add some comment

--- email_1.py
+++ email_1.py
@@ -1,17 +1,27 @@
+# -*- coding:utf-8 -*-
 from email.mime.text import MIMEText
+from email.header import Header
+from email.utils import parseaddr, formataddr
 import smtplib
-# MIMEText对象：正文 数据类型 编码
-msg = MIMEText('hejingqi, this is python email', 'plain', 'utf-8')
-msg['Subject'] = "python"
-# from_addr = input('From: ')
+
+# 处理地址编码
+def _formate_addr(s):
+    name, addr = parseaddr(s)
+    # 将中文名称编码
+    # formataddr/parseaddr处理二元元组[一个键值对，一个编码方式]
+    return formataddr((Header(name, 'utf-8').encode(), addr))
+
 from_addr = '13240363594@163.com'
-# password = input('Password: ')
-# password = '426118'
 password = 'albnwhuwneurszaj'
-# to_addr = input('To: ')
 to_addr = '502434432@qq.com'
-# smtp_server = input('SMTP server: ')
 smtp_server = 'smtp.163.com'
+
+# MIMEText对象：正文 数据类型 编码
+# msg = MIMEText('hejingqi, this is python email', 'plain', 'utf-8')
+msg = MIMEText('测试正文', 'plain', 'utf-8')
+msg['From'] = _formate_addr('Python爱好者 <%s>' % from_addr)
+msg['To'] = _formate_addr('Python管理员 <%s>' % to_addr)
+msg['Subject'] = Header('中文测试', 'utf-8').encode()
 
 server = smtplib.SMTP(smtp_server, 25)
 server.set_debuglevel(1)
