63f0151f221bc2e987530fda603d3bd87c15af53
simple0426 <perfect_0426@qq.com>
2015-12-05 19:29:50 +0800

    add some comment

--- sqlalchemy_1.py
+++ sqlalchemy_1.py
@@ -8,18 +8,22 @@ class User(Base):
     id = Column(String(20), primary_key=True)
     name = Column(String(20))
 
+# 连接表示方法：数据库类型+数据库驱动名称://用户名:密码@ip:port/数据库名
 TEST_URL = 'postgresql://postgres:123456@127.0.0.1:5432/test'
+# 初始化数据库连接
 engine = create_engine(TEST_URL)
+# 创建session类型
 DBSession = sessionmaker(bind=engine)
 """
 session = DBSession()
+# 创建user对象
 new_user = User(id='5', name='Bob')
+# 添加user对象到session
 session.add(new_user)
 session.commit()
 session.close()
 """
-session_2 = DBSession()
-user = session_2.query(User).filter(User.id=='1').one()
-print('type:', type(user))
-print('name:', user.name)
-session_2.close()
+session = DBSession()
+session.execute("insert into new values(3, 'mengke')")
+session.commit()
+session.close()
